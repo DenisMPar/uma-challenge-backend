9966b312cf1e05a72171c69305c2ba1f756ef03d
Denis <denispar.dev@gmail.com>
2024-05-19 18:06:44 -0300

    Fixed: added cors()

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import api from "./api";
+import cors from "cors";
 import { initDatabase } from "./DB/connect";
 
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use("/api/v1", api);
 
 initDatabase();
